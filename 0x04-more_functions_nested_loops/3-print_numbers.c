8e10442fc62adc585cfeee76700387935f966acf
Michael_Ekanem <michaelekanem33@gmail.com>
2023-02-23 19:52:27 -0800

    d

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	c = 0;
+	while (c < 10)
+	{
+		_putchar(c);
+		c++
+	}
+		_putchar('\n');
