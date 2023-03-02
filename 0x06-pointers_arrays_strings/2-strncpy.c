5e10d4a8828fee555f21efe10526e8e64c2d808a
Michael_Ekanem <michaelekanem33@gmail.com>
2023-03-02 04:13:22 -0800

    m

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strncpy - Write a function that copies a string.
+ * @dest: destination file.
+ * @src: source.
+ * @n: variable.
+ * Return: dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	dest[];
+	src[];
+	int i;
+
+	while (i < n)
+	{
+		for (i = 0; src[i]; i++)
+		{
+			dest[i] = src[i];
+		}
+	}
+	return (dest);
+}
