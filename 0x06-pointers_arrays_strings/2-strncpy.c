1eb46163da3a2b7ec2b67e833ac7c280ef6e6a19
Michael_Ekanem <michaelekanem33@gmail.com>
2023-03-02 04:35:37 -0800

    m

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,9 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	dest[];
-	src[];
 	int i;
 
-	while (dest != '\0' && src != '\0')
-		i++;
-	for (i = 0; src[i]; i++)
+	for (i = 0; i < n && src[i]; i++)
 	{
 		dest[i] = src[i];
 	}
