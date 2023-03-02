95b30af9b6ee47d366e067cfd7585c36e90fb068
Michael_Ekanem <michaelekanem33@gmail.com>
2023-03-02 14:41:02 -0800

    m

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,12 +9,10 @@ char *string_toupper(char *n)
 {
 	int i;
 
-	i = 0;
-	while (n[i])
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		if ((n[i] >= 97) && (n[i] <= 122))
 			n[i] -= 32;
-		i++;
 	}
 
 	return(n);
