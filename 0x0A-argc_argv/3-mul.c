05c02ee23907cfdcec91b2f7b531175b0b3adefd
Michael_Ekanem <michaelekanem33@gmail.com>
2023-03-11 20:32:25 -0800

    m

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that multiplies two numbers.
+ * @num1: first number
+ * @num2: second number.
+ * Return: 0.
+ */
+
+int main(int num1, int num2)
+{
+	num1 = 2;
+	num2 = 3;
+	int mul;
+
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (0);
+}
