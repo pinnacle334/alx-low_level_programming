3e5a077f0dadd06a38b0ecb1479956a807062f4b
Michael_Ekanem <michaelekanem33@gmail.com>
2023-03-11 19:51:56 -0800

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,8 @@
  * Return: 0.
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	int i = 0;
-
-	printf("%s\n", argv[i]);
+	printf("%s\n", *argv);
 	return (0);
 }
