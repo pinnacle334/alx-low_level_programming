e48eabe7dd8ec3fe1f5afc2169d2e2903c9698a3
Michael_Ekanem <michaelekanem33@gmail.com>
2023-03-11 19:34:09 -0800

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a function that prints its name.
+ * @argc: varible
+ * @argv: string
+ * Return: 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", argv);
+	return 0;
+}
