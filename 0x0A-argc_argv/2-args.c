3ac684a27d85285050c78e43a6da94632b790cb7
Michael_Ekanem <michaelekanem33@gmail.com>
2023-03-11 20:16:59 -0800

    n

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that prints all arguments.
+ * @argc: no. of arg
+ * @argv: array of arg.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (o);
+}
