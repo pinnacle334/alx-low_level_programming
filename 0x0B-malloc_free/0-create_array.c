c63eef8e0ae8e07733220fe5ccd84a5d4d90edba
Michael_Ekanem <michaelekanem33@gmail.com>
2023-03-13 17:36:34 -0700

    m

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,6 +19,8 @@ char *create_array(unsigned int size, char c)
 	scanf("%u", &size);
 	arr = (char *)malloc(size * sizeof(char));
 
+	for (size == 0)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
