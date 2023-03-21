459fcf892114e2bb5efb1d51bea7a2cf393db243
Michael_Ekanem <michaelekanem33@gmail.com>
2023-03-21 07:45:59 -0700

    m

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define _FILE_
+
+/**
+ * main - a program that prints the name of the file
+ *
+ * Return: Always ).
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
