99cdb4298dc2130636216698fadd305bc77a9096
Michael Ekanem <michaelekanem33@gmail.com>
2023-07-07 14:12:59 -0700

    m

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * binary_to_unit - A function that converts a binary
+ * number to an unsigned int.
+ * @b: pointer variable
+ * Return: Always 0 (Success).
+ */
+
+unsigned int binary_to_unit(const char *b)
+{
+unsigned int result;
+int lent;
+int i;
+
+result = 0;
+lent = strlen(b);
+
+for (i = lent - 1; i >= 0; i--)
+{
+	if (b[i] == '1')
+	{
+		result = result + pow(2, lent - i - 1);
+	}
+}
+
+return (result);
+}
