e05878d58ae7a3975ed4df485f6edf928351d083
Michael Ekanem <michaelekanem33@gmail.com>
2023-07-08 03:06:32 -0700

    mee

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - a func that returns the value of a bit at a given index
+ * @num_b: num looked for
+ * @dex:the bit index
+ *
+ * Return:value of bit
+ */
+int get_bit(unsigned long int num_b, unsigned int dex)
+{
+	int bits;
+
+	if (dex > 63)
+		return (-1);
+
+	bits = (num_b >> dex) & 1;
+
+	return (bits);
+}
