539fd1b420f05cc03017adb7200a7bdf55e44819
Michaek Ekanem <michaelekanem33@gmail.com>
2024-03-05 08:38:06 -0800

    m

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,28 +7,15 @@
  */
 
 void puts_half(char *str)
-
 {
-        int j = 0;
-        int k;
+	int i, k;
 
-        while (str[j] != '\0')
-        {
-        j++;
-        }
-        if (j % 2 == 1)
-        {
-        j = (j - 1) / 2;
-        j += 1;
-        }
-        else
-        {
-        j = j / 2;
-        }
-
-        for (k = 0; k < j; k++)
-        {
-        _putchar(str[k]);
-       	_putchar('\n');
+	for (i = 0; str[i] != '\0'; i++)
+	{
 	}
+	k = i/2;
+
+	for (; str[k] != '\0'; k++)
+		_putchar(str[k];
+	_putchar('\n');
 }
