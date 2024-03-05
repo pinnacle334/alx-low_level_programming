13c89c823a9d805332f8962f10f4a2d6c24e69e3
Michaek Ekanem <michaelekanem33@gmail.com>
2024-03-05 08:11:27 -0800

    m

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,19 +7,13 @@
  */
 
 void puts2(char *str)
-        
-{       
-        int i;
-        int j = 0;
-        
-        while (str[j] != '\0')
-        {       
-        j++;    
-        }
-        
-        for (i = 0; i < j; i += 2)
-        {       
-        _putchar(str[i]);
-        }
-        _putchar('\n');
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+       _putchar('\n');
 }
